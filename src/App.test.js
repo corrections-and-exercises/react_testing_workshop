385680b39e3d238f1dc41f20482c70bf4eebb2af
patrick-plue <patrick.pluequett@wbscodingschool.com>
2023-04-24 12:07:28 +0200

    add second test

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,17 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App.js';
 
-test('simulate button click', async () => {
-  render(<App />);
-  const button = screen.getByRole('button');
-  await userEvent.click(button);
-  const headline = screen.getByText(/hello world/i);
-  expect(headline).toBeInTheDocument();
+describe('testing headline', () => {
+  test('if headline is initially not shown', () => {
+    render(<App />);
+    const headline = screen.queryByText(/hello world/i);
+    expect(headline).toBeNull();
+  });
+  test('if headline is shown after button click', async () => {
+    render(<App />);
+    const button = screen.getByRole('button');
+    await userEvent.click(button);
+    const headline = screen.getByText(/hello world/i);
+    expect(headline).toBeInTheDocument();
+  });
 });
